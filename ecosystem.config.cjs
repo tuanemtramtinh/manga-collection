e44ba17d2779ae0b91a833b8c8cb5c951e88ee94
Nguyễn Tuấn Anh <117497476+tuanemtramtinh@users.noreply.github.com>
2026-08-29 11:51:48 +0700

    add pm2

---
+++ ecosystem.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: 'my-app',
+      script: './dist/index.js',
+      instances: 1,
+      exec_mode: 'fork',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '500M',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+    },
+  ],
+}
